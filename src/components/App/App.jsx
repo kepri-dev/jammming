d76be490fdaff16f1f648a82a736976836240e5f
Ali <ali@outana.fr>
2023-11-20 11:37:44 +0100

    css added and App written

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,19 +1,57 @@
-import React from "react";
-import TrackList from '../TrackList/TrackList';
-import Track from "../Track/Track";
-import SearchBar from '../SearchBar/SearchBar';
-import SearchResults from '../SearchResults/SearchResults';
-import Playlist from '../Playlist/Playlist';
-
-
+import React, { useState } from 'react';
+import SearchBar from './SearchBar/SearchBar';
+import SearchResults from './SearchResults/SearchResults';
+import Playlist from './Playlist/Playlist';
+import Spotify from './Spotify/Spotify';
 
 function App() {
-    return (
-        <div>
-            <h1>Jammming</h1>
+  const [searchResults, setSearchResults] = useState([]);
+  const [playlistName, setPlaylistName] = useState('New Playlist');
+  const [playlistTracks, setPlaylistTracks] = useState([]);
 
-        </div>
-    )
+  const addTrack = (track) => {
+    if (!playlistTracks.find(savedTrack => savedTrack.id === track.id)) {
+      setPlaylistTracks([...playlistTracks, track]);
+    }
+  };
+
+  const removeTrack = (track) => {
+    setPlaylistTracks(playlistTracks.filter(savedTrack => savedTrack.id !== track.id));
+  };
+
+  const updatePlaylistName = (name) => {
+    setPlaylistName(name);
+  };
+
+  const savePlaylist = () => {
+    const trackURIs = playlistTracks.map(track => track.uri);
+    Spotify.savePlaylist(playlistName, trackURIs).then(() => {
+      setPlaylistName('New Playlist');
+      setPlaylistTracks([]);
+    });
+  };
+
+  const search = (term) => {
+    Spotify.search(term).then(searchResults => {
+      setSearchResults(searchResults);
+    });
+  };
+
+  return (
+    <div>
+      <SearchBar onSearch={search} />
+      <div className="App-playlist">
+        <SearchResults searchResults={searchResults} onAdd={addTrack} />
+        <Playlist 
+          playlistName={playlistName} 
+          playlistTracks={playlistTracks} 
+          onRemove={removeTrack} 
+          onNameChange={updatePlaylistName} 
+          onSave={savePlaylist} 
+        />
+      </div>
+    </div>
+  );
 }
 
 export default App;
